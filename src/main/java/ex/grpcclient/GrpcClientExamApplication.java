b61167c7c01b3aee856571b934903378b9a9daff
bjp <bj11077@metabuild.co.kr>
2024-09-13 09:12:28 +0900

    init

---
+++ src/main/java/ex/grpcclient/GrpcClientExamApplication.java
@@ -0,0 +1,13 @@
+package ex.grpcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcClientExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcClientExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ex/grpcclient/GrpcClientExamApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.grpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
